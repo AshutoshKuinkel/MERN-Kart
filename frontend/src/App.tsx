ab302f141b93f51fdb837998c64ed5978b31d3a0
Ashutosh Kuinkel <108271619+hejjwheh@users.noreply.github.com>
2025-08-11 10:42:56 +1000

    sign up + login form frontend setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css'
+import Signup from './pages/auth/signup'
 import Login from './pages/auth/login'
 
 const App = () => {
   return (
     <main className='h-full'>
-      <h1>Hello World</h1>
+      {/* <h1>Hello World</h1> */}
       <Login/>
+      <Signup/>
     </main>
   )
 }
