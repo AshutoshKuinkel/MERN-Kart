992799babe1a4f22ba508c1a4304b0aa989625aa
Ashutosh Kuinkel <108271619+hejjwheh@users.noreply.github.com>
2025-08-17 15:11:26 +1000

    react-hook-form for signup page

--- frontend/src/components/forms/singup.form.tsx
+++ frontend/src/components/forms/singup.form.tsx
@@ -1,12 +1,12 @@
-import { LuAsterisk } from "react-icons/lu";
 import { yupResolver } from "@hookform/resolvers/yup";
-import { useForm } from "react-hook-form";
+import { FormProvider, useForm } from "react-hook-form";
 import { signupSchema } from "../../schema/auth.schema";
 import type { ISignupData } from "../../types/auth.types";
+import Input from "../common/inputs/input";
 
 const SignupForm = () => {
 
-  const {register,handleSubmit,watch,formState:{errors}} = useForm({
+  const methods = useForm({
     defaultValues:{
       firstName:'',
       lastName:'',
@@ -23,93 +23,51 @@ const SignupForm = () => {
   }
 
   return (
-   <form onSubmit={handleSubmit(onSubmit)}>
+    <FormProvider {...methods}>
+         <form onSubmit={methods.handleSubmit(onSubmit)} autoComplete="off">
      <div>
 
-        <div className="mt-5 flex flex-col gap-1">
-          <div className="flex">
-            <label htmlFor="firstName" className="text-gray-800 font-semibold text-lg">First Name:</label>
-            <LuAsterisk  size = {14} className="text-red-500"/>
-          </div>
-          <input id={'firstName'} {...register('firstName')} value={watch('firstName')} 
-          className={
-            `px-2 py-2 border rounded-md ${errors.firstName
-              ? 'border-red-500  focus:outline-red-500' 
-              :  'border-violet-400 focus:outline-violet-600'}`}
-          placeholder="Your First Name" autoComplete="none"
-           
+        <div className="mt-5 flex flex-col gap-4">
+          <Input
+            label="First Name:"
+            id="firstName"
+            name="firstName"
+            placeholder="Your Name"
+            required
           />
-          {
-              errors.firstName && <p className="text-red-500 text-xs">{errors.firstName ? errors.firstName.message : ''}</p>
-          }
-        </div>
-        
-        <div className="mt-5 flex flex-col gap-1">
-          <div className="flex">
-            <label htmlFor="lastName" className="text-gray-800 font-semibold text-lg">Last Name:</label>
-            <LuAsterisk  size = {14} className="text-red-500"/>
-          </div>
-          <input id={'lastName'} {...register('lastName')} value={watch('lastName')} 
-          className={
-            `px-2 py-2 border rounded-md ${errors.lastName
-              ? 'border-red-500  focus:outline-red-500' 
-              :  'border-violet-400 focus:outline-violet-600'}`}
-          placeholder="Your Last Name" autoComplete="none"
-          />
-          {
-              errors.lastName && <p className="text-red-500 text-xs">{errors.lastName ? errors.lastName.message : ''}</p>
-          }
-        </div>
 
-        <div className="mt-5 flex flex-col gap-1">
-          <div className="flex">
-            <label htmlFor="email" className="text-gray-800 font-semibold text-lg">Email:</label>
-            <LuAsterisk  size = {14} className="text-red-500"/>
-          </div>
-          <input id={'email'} {...register('email')} value={watch('email')} 
-          className={
-            `px-2 py-2 border rounded-md ${errors.email 
-              ? 'border-red-500  focus:outline-red-500' 
-              :  'border-violet-400 focus:outline-violet-600'}`}
-          placeholder="example@gmail.com"
+          <Input
+            label="Last Name:"
+            id="lastName"
+            name="lastName"
+            placeholder="Your Name"
+            required
           />
-          {
-              errors.email && <p className="text-red-500 text-xs">{errors.email ? errors.email.message : ''}</p>
-          }
-        </div>
 
-        <div className="mt-5 flex flex-col gap-1">
-          <div className="flex">
-            <label htmlFor="password" className="text-gray-800 font-semibold text-lg">Password:</label>
-            <LuAsterisk  size = {14} className="text-red-500"/>
-          </div>
-          <input id={'password'} {...register('password')} value={watch('password')} 
-          className={
-           `px-2 py-2 border rounded-md ${errors.password 
-             ? 'border-red-500  focus:outline-red-500' 
-             :  'border-violet-400 focus:outline-violet-600'}`}
-          placeholder="********" type="password" autoComplete="none"
+          <Input
+            label="Email:"
+            id="email"
+            name="email"
+            placeholder="example@gmail.com"
+            required
           />
-          {
-              errors.password && <p className="text-red-500 text-xs">{errors.password ? errors.password.message : ''}</p>
-          }
-        </div>
 
-        <div className="mt-5 flex flex-col gap-1">
-          <div className="flex">
-            <label htmlFor="phone" className="text-gray-800 font-semibold text-lg">Phone:</label>
-            <LuAsterisk  size = {14} className="text-red-500"/>
-          </div>
-          <input id={'phone'} {...register('phone')} value={watch('phone')} 
-          className={
-           `px-2 py-2 border rounded-md ${errors.phone
-             ? 'border-red-500  focus:outline-red-500' 
-             :  'border-violet-400 focus:outline-violet-600'}`}
-          placeholder="#########" autoComplete="none"
+          <Input
+            label="Password:"
+            id="password"
+            name="password"
+            placeholder="********"
+            required
+            type="password"
           />
-          {
-              errors.phone && <p className="text-red-500 text-xs">{errors.phone ? errors.phone.message : ''}</p>
-          }
+
+          <Input
+            label="Phone:"
+            id="phone"
+            name="phone"
+            placeholder="##########"
+            required
+          />
         </div>
 
         <div className="mt-6 w-full">
@@ -118,6 +76,7 @@ const SignupForm = () => {
 
       </div>
    </form>
+    </FormProvider>
   )
 }
 
--- frontend/src/schema/auth.schema.ts
+++ frontend/src/schema/auth.schema.ts
@@ -2,14 +2,14 @@
 import * as yup from "yup";
 
 export const loginSchema = yup.object({
-  email:yup.string().required('email is required.').email('invalid email format'),
-  password:yup.string().required('password is required')
+  email:yup.string().required('Email is required.').email('Invalid email format'),
+  password:yup.string().required('Password is required')
 })
 
 export const signupSchema = yup.object({
   firstName:yup.string().required('First Name is required.'),
   lastName:yup.string().required('Last Name is required.'),
-  email:yup.string().required('email is required.').email('invalid email format'),
-  password:yup.string().required('password is required'),
-  phone:yup.string().required('phone is required.')
+  email:yup.string().required('Email is required.').email('Invalid email format'),
+  password:yup.string().required('Password is required'),
+  phone:yup.string().required('Phone is required.')
 })
