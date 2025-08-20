5ed2eadd8e6e3de974a53807b345c93418aaefcf
Ashutosh Kuinkel <108271619+hejjwheh@users.noreply.github.com>
2025-08-20 19:50:55 +1000

    integration of backend within frontend

--- frontend/src/api/auth.api.ts
+++ frontend/src/api/auth.api.ts
@@ -4,7 +4,7 @@ import type { ILoginData } from '../types/auth.types';
 export const login = async(data:ILoginData)=>{
   try{
 
-    const response = await axios.post('https://mern-kart-56yx.onrender.com/api/auth/login')
+    const response = await axios.post('https://mern-kart-56yx.onrender.com/api/auth/login',data)
     console.log(response)
     return response.data
 
